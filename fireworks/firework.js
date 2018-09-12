1b9e911cab5c254f6993695ec731facd7b1871aa
AminLer <aminoumhamdi@gmail.com>
2018-09-12 13:26:56 +0200

    progress

--- fireworks/firework.js
+++ fireworks/firework.js
@@ -1,13 +1,30 @@
-function Firework(x, y){
-  this.pos = createVector(x, y);
-  this.r = 14;
+function Firework(){
+  this.pos = createVector(random(10, width-10), random(height, height*3));
+  this.r = 20;
+  this.vel = createVector(0, 0);
+  this.accl = createVector(0, -8);
+  this.gravity = createVector(0, 0.05);
+  this.update = function(){
+    this.vel.add(this.accl);
+    this.pos.add(this.vel);
+    this.accl.mult(0);
+    if(this.pos.y < height*1.5) this.vel.add(this.gravity);
+    this.bondery();
+  }
+
+  this.bondery = function(){
+    if(this.vel.y > -1){
+      return true;
+    }
+  }
+
 
   this.show = function(){
-    fill(255, 0, 100);
+    fill(255, 0, 100, 255);
     noStroke();
-    ellipse(this.pos.x, this.pos.y, this.r/3, this.r/2);
-    ellipse(this.pos.x, this.pos.y, this.r/4);
-    ellipse(this.pos.x, this.pos.y, this.r/6);
-    ellipse(this.pos.x, this.pos.y, this.r/6);
+    ellipse(this.pos.x, this.pos.y, this.r/3, this.r/1.7);
+    ellipse(this.pos.x, this.pos.y+this.r/2, this.r/5, this.r/4);
+    ellipse(this.pos.x, this.pos.y+this.r*(0.74), this.r/7, this.r/6);
+    ellipse(this.pos.x, this.pos.y+this.r*(0.94), this.r/7, this.r/6);
   }
 }
--- fireworks/sketch.js
+++ fireworks/sketch.js
@@ -1,10 +1,41 @@
-let firework;
+let firework = [];
+let firework2;
+let length = 10;
+let particle;
+let particles = [];
 function setup() {
   createCanvas(900, 600);
-  firework = new Firework(width/2, height/2);
+  for(let i = 0; i < length; i++){
+    //particles = new Particle(0, 0, 0, 0, 0, 0);
+    firework[i] = new Firework();
+  }
+}
+
+function mousePressed(){
+  //firework2 = new Firework();
+  console.log("pressed");
 }
 
 function draw() {
-  background(100);
-  firework.show();
+  background(100, 50);
+  for(let i = length-1; i >=0; i--){
+    firework[i].show();
+    firework[i].update();
+    if(firework[i].bondery()){
+      particles[i] = new Particle(firework[i].pos.x, firework[i].pos.y, 2, 40, 1, random(5))
+      firework[i] = new Firework();
+    }
+    if(particles[i]) particles[i].update();
+  }
+
+  if(firework2){
+      firework2.show();
+      firework2.update();
+      if(firework2.bondery()){
+        particle = new Particle(firework2.pos.x, firework2.pos.y,2, 50, 1, random(0, 1));
+        firework2 = new Firework();
+      }
+  }
+  if(particle) particle.update();
+
 }
